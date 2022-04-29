549afdc94b3106b280810b0f71017d55f64d45ca
DharmikPansuriya <76813316+DharmikPansuriya@users.noreply.github.com>
2022-04-29 21:50:28 +0530

    Minor Change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      {/* <Route path="/" component={}/> */}
+      <Route path="/"/>
       <Route path="/chats" />
     </div>
   );
